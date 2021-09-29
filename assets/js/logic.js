57e46009f0360b298019eeaff9825881e8bd4739
JC Renner <jc-renner@lu-tec.com>
2021-09-28 21:40:01 -0400

    starting code plus questions array

---
+++ assets/js/logic.js
@@ -0,0 +1,41 @@
+var time = questions.length * 15;
+var timerId;
+var questionIndex = 0;
+
+
+var startBtn = document.querySelector('#start-btn');
+var questionElement = document.querySelector('#questions');
+var timerElement = document.querySelector('#time');
+var questionChoices = document.querySelector('#choices');
+
+
+// start game
+function startGame() {
+    // hide start screen
+    var startScreen = document.querySelector('#start-screen');
+    startScreen.setAttribute('class', 'hide');
+
+    //show questions
+    questionElement.removeAttribute('class');
+    getCurrentQuestion();
+}
+
+function getCurrentQuestion() {
+    var currentQuestion = questions[questionIndex];
+    console.log(currentQuestion);
+    var titleElement = document.querySelector('#question-title');
+    titleElement.textContent = currentQuestion.title;
+    questionChoices.textContent = '';
+
+    for(var i = 0; i <currentQuestion.choice.length; i++) [
+        let choicesButton = document.createElement('button');
+        choicesButton.setAttribute('class', 'choice');
+        choicesButton.setAttribute('value', currentQuestion.choice[i]);
+
+        choicesButton.textContent = i + 1 + ". " + currentQuestion.choice[i];
+        questionChoices.appendChild(choices);
+
+    ]
+}
+
+startBtn.addEventListener('click', startGame)
---
+++ assets/js/questions.js
@@ -0,0 +1,77 @@
+var questions = [
+{
+    title: `What is the name of the CSS property used to control the appearance of everything behind all the elements on the page?`,
+    choice: [
+        `display`, `viewport`, `background`, `underlayer`
+    ],
+    answer: `background`
+},
+{
+    title: `What declaration must be included as the first item in an HTML document and is used to inform the web browser what type of document is to be displayed?`,
+    choice: [
+        `<meta>`, `<html>`, `<document>`, `<!DOCTYPE>`
+    ],
+    answer: `<!DOCTYPE>`
+},
+{
+    title: `True or False: The external JavaScript file must contain the <script> tag.`,
+    choice: [
+        `True`, `False`
+    ],
+    answer: `False`
+},
+{
+    title: `Which element tag do we use to add our JavaScript?`,
+    choice: [
+        `<js>`, `<JavaScript>`, `<script>`, `<addscript>`, `<link>`
+    ],
+    answer: `<script>`
+},
+{
+    title: `The link element referencing a website's CSS file must go inside the _________ section of the HTML document`,
+    choice: [
+        `<body>`, `<footer>`, `<meta>`, `<head>`
+    ],
+    answer: `<head>`
+},
+{
+    title: `How do you create a function in JavaScript?`,
+    choice: [
+        `function = myFunction()`, `function myFunction()`, `func myFunction()`, `var.function = myFunction()`
+    ],
+    answer: `function myFunction()`
+},
+{
+    title: `Which JavaScript could we use to change the text content of the example below\n
+        <h1 id='sample-id'>Change this sample text.</h1>`,
+    choice: [
+        `document.getElementById('sample-id').innerHTML = 'This is my new text.';`,
+        `document.getElement('h1').innerHTML = 'This is my new text.';`,
+        `document.getElementByClass('sample-id').innerText = 'This is my new text.';`,
+        `sample-id.innerText('This is my new text.)`
+    ],
+    answer: `document.getElementById('sample-id').innerHTML = 'This is my new text.';`
+},
+{
+    title: `What is the correct way to refer to an external script called "style.js"?`,
+    choice: [
+        `<script src='style.js'>`, `<script href='style.js'>`, `javascript src='style'>`, `<js href='style'>`
+    ],
+    answer: `<script src='style.js'>`
+},
+{
+    title: `What tag is used to create an unordered list?`,
+    choice: [
+        `<unordered>`, `<ol>`, `<list>`, `<ul>`
+    ],
+    answer: `<ul>`
+},
+{
+    title: `What tag is used to define or add an image to an HTML page?`,
+    choice: [
+        `<image>`, `<a href>`, `<img src>`, `<img>`
+    ],
+    answer: `<img>`
+}
+
+]
