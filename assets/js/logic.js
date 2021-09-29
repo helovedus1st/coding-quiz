775367e49de97f0ad9ef3d4d031181915905a1f8
JC Renner <jc-renner@lu-tec.com>
2021-09-29 09:22:00 -0400

    bug fixing

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,6 +7,7 @@ var startBtn = document.querySelector('#start-btn');
 var questionElement = document.querySelector('#questions');
 var timerElement = document.querySelector('#time');
 var questionChoices = document.querySelector('#choices');
+var choicesDiv = document.querySelector('#choices-div');
 
 
 // start game
@@ -27,15 +28,15 @@ function getCurrentQuestion() {
     titleElement.textContent = currentQuestion.title;
     questionChoices.textContent = '';
 
-    for(var i = 0; i <currentQuestion.choice.length; i++) [
+    for(var i = 0; i < currentQuestion.choice.length; i++) {
         let choicesButton = document.createElement('button');
-        choicesButton.setAttribute('class', 'choice');
+        choicesButton.setAttribute('class', 'choices');
         choicesButton.setAttribute('value', currentQuestion.choice[i]);
 
         choicesButton.textContent = i + 1 + ". " + currentQuestion.choice[i];
-        questionChoices.appendChild(choices);
+        questionElement.appendChild(choices);
 
-    ]
+    }
 }
 
 startBtn.addEventListener('click', startGame)
